0d2b9255d9ea45758d017e1aa5090eb28dbd2e1d
lokesh-khanna <119606216+lokesh-khanna@users.noreply.github.com>
2025-03-24 14:53:17 +0530

    Create kaushik.java

---
+++ kaushik/kaushik.java
@@ -0,0 +1,19 @@
+    System.out.print("Enter a string: ");
+    String str = scanner.nextLine();
+    
+    str = str.toLowerCase();
+    
+    String reversedStr = "";
+
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversedStr += str.charAt(i);
+    }
+    if (str.equals(reversedStr)) {
+        System.out.println("The string is a palindrome.");
+    } else {
+        System.out.println("The string is not a palindrome.");
+    }
+    
+    scanner.close();
+}
